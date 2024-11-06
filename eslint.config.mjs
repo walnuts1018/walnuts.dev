7403ab851a6087a4f26fa723f76ac34d231fad67
walnuts1018 <r.juglans.1018@gmail.com>
2024-11-06 09:08:30 +0900

    add
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { fixupConfigRules } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:import/recommended",
+    "plugin:import/warnings",
+)), {
+    rules: {
+        "import/order": ["warn", {
+            alphabetize: {
+                order: "asc",
+                caseInsensitive: true,
+            },
+        }],
+    },
+}];
