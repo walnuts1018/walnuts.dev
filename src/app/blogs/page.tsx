4ef9fca05c38a6cce0ce79ae2263abece9fad5cb
Walnuts <r.juglans.1018@gmail.com>
2024-11-22 18:33:47 +0900

    Update page.tsx

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,7 +3,7 @@ import { ContentHeader } from "@/components/ContentHeader";
 export default function BlogsPage() {
   return (
     <main className="flex flex-col items-center justify-center px-6 lg:px-10 lg:py-6  pb-24">
-      <div className="flex flex-col gap-8 max-w-full lg:max-w-[50%] 2xl:max-w-[66.6%]">
+      <div className="flex flex-col gap-8 max-w-full lg:max-w-[66.6%]">
         <ContentHeader initialText="Blog" finalText="ブログ" />
         <div className="flex flex-col gap-20 ">
           <Blogs />
