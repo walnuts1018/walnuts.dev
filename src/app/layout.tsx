02c939920929513a233e194964a9060987462a8c
walnuts1018 <r.juglans.1018@gmail.com>
2024-12-30 18:36:56 +0900

    metadata: add Open Graph image path
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,7 @@ export const metadata: Metadata = {
     siteName: defaultTitle,
     locale: "ja_JP",
     type: "website",
+    images: "/opengraph-image.jpg",
   },
   twitter: {
     card: "summary_large_image",
